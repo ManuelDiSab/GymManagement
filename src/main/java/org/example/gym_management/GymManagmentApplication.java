ce31562513c5551c2fcfc1755553ab388ea1d42f
Manuel <manuelds0094@gmail.com>
2026-05-08 15:32:02 +0200

    feat: implement environment variable management using Dotenv

--- src/main/java/org/example/gym_management/GymManagmentApplication.java
+++ src/main/java/org/example/gym_management/GymManagmentApplication.java
@@ -1,5 +1,6 @@
 package org.example.gym_management;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GymManagmentApplication {
 
     public static void main(String[] args) {
+        // Carica il file app.env | Load the app.env file
+        Dotenv dotenv = Dotenv.configure()
+                .filename("app.env") // Specifica il nome se non è il classico .env | Specify the name if it's not classic .env
+                .load();
+
+        // Le espone come proprietà di sistema per Spring | Makes it a system property
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(GymManagmentApplication.class, args);
     }
 
---
+++ src/main/java/org/example/gym_management/repositories/MembershipRepository.java
@@ -0,0 +1,21 @@
+package org.example.gym_management.reposiotries;
+
+import jakarta.transaction.Transactional;
+import org.example.gym_management.entities.Membership;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Repository
+public interface MembershipRepository extends JpaRepository<Membership,Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Membership m SET m.active = false WHERE m.endDate < :today AND m.active = true")
+    void deactivateMembership(@Param("today")LocalDate today);
+}
